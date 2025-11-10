f03f223e3fc00abcb3c478227f8491669f78f6db
qkrxogmla <suk05553@naver.com>
2025-11-10 23:57:12 +0900

    fix: header 데스크탑 버전 버튼 인터렉션 수정 및 hero section 반응형 수정

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        border: "var(--border)",
+        ring: "var(--ring)",
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+};
